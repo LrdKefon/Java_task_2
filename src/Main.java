365c05f00df65d08e3c13365df39c052dd1ebaf9
Alex <zazerny@gmail.com>
2023-02-08 18:41:43 +0300

    Task_2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int account_1 = 100; // баланс
+        int x = 1200;        // сумма пополнения
+        int account_2 = x + account_1;
+        int bonus;
+        if ( x >= 1000 ) {
+            bonus = x / 100;
+        }else {
+            bonus = 0;
+        }
+        int total_bonus = bonus;
+        System.out.println("Your balance: " + account_2 + " rubles");
+        System.out.println("You've received: " + total_bonus + " bonus.");
+
     }
 }
